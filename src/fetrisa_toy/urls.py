3e44b07e916dae95a02dd6499ad75cdb91718099
darling1864 <dashaalisa2003@mail.ru>
2023-03-29 18:28:11 +0300

    add pattern_store app

--- src/fetrisa_toy/urls.py
+++ src/fetrisa_toy/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("store.urls")),
+    path("", include("main_store.urls")),
     path("admin/", admin.site.urls),
 ]
---
+++ src/pattern_store/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PatternStoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pattern_store'
