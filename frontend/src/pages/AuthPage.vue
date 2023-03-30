7b91890bd87913bf0f03db1bdd95cd7312c55422
darling1864 <dashaalisa2003@mail.ru>
2023-03-30 13:41:56 +0300

    correct page of auth

--- frontend/src/pages/AuthPage.vue
+++ frontend/src/pages/AuthPage.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class='form'>
-    <form method="post">
-      <div class="container">
-        <h1>Регистрация</h1>
-        <hr>
+  <div class="col-md-4 offset-md-4">
+    <div class="form-container">
+      <div class="form-icon"><i class="fa fa-user"></i></div>
+      <h3 class="title">Регистрация</h3>
+      <form class="form-horizontal">
+        <div class="form-group">
+          <hr>
+          <br>
+          <label for="email"><b>Email: </b></label>{{ ' ' }}
+          <input class="form-control" name="email" type="text" v-model="email"/>
+          <br>
+          <br>
+          <label for="psw"><b>Пароль: </b></label> {{ ' ' }}
+          <input class="form-control" name="psw" type="password" v-model="psw"/>
+          <br>
+          <br>
+          <label for="psw-repeat"><b>Повторите пароль: </b></label> {{ ' ' }}
+          <input class="form-control" name="psw_repeat" type="password" v-model="psw_repeat"/>
+          <br>
+          <br>
+          <br>
+          <button type="submit" class="btn btn-default">Регистрация</button>
+        </div>
         <br>
-        <label for="email"><b>Email: </b></label>{{ ' ' }}
-        <input name="email" type="text" v-model="email"/>
-        <br>
-        <br>
-        <label for="psw"><b>Пароль: </b></label> {{ ' ' }}
-        <input name="psw" type="password" v-model="psw"/>
-        <br>
-        <br>
-        <label for="psw-repeat"><b>Повторите пароль: </b></label> {{ ' ' }}
-        <input name="psw_repeat" type="password" v-model="psw_repeat"/>
-        <br>
-        <br>
-        <button type="submit" class="registerbtn">Регистрация</button>
-      </div>
-      <br>
-      <div class="container signin">
-        <p>У вас уже есть аккаунт?<a href="/menu/login/"> Войти</a>.</p>
-      </div>
-    </form>
+        <div class="container signin">
+          <p>У вас уже есть аккаунт?<a href="/menu/login/"> Войти</a>.</p>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -41,51 +45,87 @@ export default {
 </script>
 
 <style scoped>
-
-.form {
-  background: #fcecde;
-  font-family: 'Nunito', sans-serif;
-  width: 500px;
-  padding: 40px;
-  border-radius: 10px;
-  margin: 30px auto 15px;
-  box-shadow: 14px 14px 20px #cbced1, -14px -14px 20px white;
+.form-container{
+ background: #FAE8DC;
+ font-family: 'Nunito', sans-serif;
+ padding: 40px;
+ border-radius: 20px;
+ box-shadow: 14px 14px 20px #E6B493, -14px -14px 20px #ffffff;
 }
 
-.form-container .form-horizontal .form-group label {
-  font-size: 15px;
-  font-weight: 600;
-  text-transform: uppercase;
+.form-container .form-icon{
+ color: #d39a74;
+ font-size: 55px;
+ text-align: center;
+ line-height: 100px;
+ width: 100px;
+ height:100px;
+ margin: 0 auto 15px;
+ border-radius: 50px;
+ box-shadow: 7px 7px 10px #d3a688, -7px -7px 10px #ffffff;
 }
 
-button {
-  color: #000;
-  background-color: #ffe3ca;
-  font-size: 15px;
-  font-weight: bold;
-  text-transform: uppercase;
-  width: 100%;
-  padding: 18px 79px 15px;
-  border-radius: 25px;
-  box-shadow: 6px 6px 6px #c9c8c8, -6px -6px 6px #ffffff;
-  border: none;
-  transition: all 0.5s ease 0s;
+.form-container .title{
+ color: #d39a74;
+ font-size: 25px;
+ font-weight: 700;
+ text-transform: uppercase;
+ letter-spacing: 1px;
+ text-align: center;
+ margin: 0 0 20px;
 }
 
-button:hover,
-button:focus {
-  color: #000000;
-  letter-spacing: 4px;
-  box-shadow: none;
-  outline: none;
+.form-container .form-horizontal .form-group{ margin: 0 0 25px 0; }
+
+.form-container .form-horizontal .form-group label{
+ font-size: 15px;
+ font-weight: 600;
+ text-transform: uppercase;
 }
 
-form {
-  height: 10px;
-  padding: 20px;
-  display: inline-block;
-  transition: all 0.3s ease 0s;
-  outline: none;
-  color: #000000;
+.form-container .form-horizontal .form-control{
+ color: #333;
+ background: #ffffff;
+ font-size: 15px;
+ height: 50px;
+ padding: 20px;
+ letter-spacing: 1px;
+ border: none;
+ border-radius: 50px;
+ box-shadow: inset 6px 6px 6px #ffffff, inset -6px -6px 6px #f3dbcc;
+ display: inline-block;
+ transition: all 0.3s ease 0s;
+}
+
+.form-container .form-horizontal .form-control:focus{
+ box-shadow: inset 6px 6px 6px #ffffff, inset -6px -6px 6px #d3a688;
+ outline: none;
+}
+
+.form-container .form-horizontal .form-control::placeholder{
+ color: #ffffff;
+ font-size: 14px;
+}
+
+.form-container .form-horizontal .btn{
+ color: #000;
+ background-color: #ffffff;
+ font-size: 15px;
+ font-weight: bold;
+ text-transform: uppercase;
+ width: 100%;
+ padding: 12px 15px 11px;
+ border-radius: 20px;
+ box-shadow: 6px 6px 6px #d3a688, -6px -6px 6px #ffffff;
+ border: none;
+ transition: all 0.5s ease 0s;
+}
+
+.form-container .form-horizontal .btn:hover,
+.form-container .form-horizontal .btn:focus{
+ color: #fff;
+ letter-spacing: 3px;
+ box-shadow: none;
+ outline: none;
 }
 </style>
