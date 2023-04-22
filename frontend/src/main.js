8253cebdc2e6903201ac264c91673fc928ba52e4
easydush <easydush@gmail.com>
2023-04-22 20:56:41 +0300

    fixes bootstrap import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,11 @@ import { createRouter, createWebHistory } from "vue-router"
 import App from './App.vue'
 import AuthPage from "@/pages/AuthPage";
 import LoginPage from "@/pages/LoginPage.vue";
+import { BootstrapVue } from 'bootstrap-vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const routeInfos = [
     {
@@ -23,4 +27,4 @@ const router = createRouter({
 
 const app = createApp(App)
 
-app.use(router).mount('#app')
+app.use(router).use(BootstrapVue).mount('#app')
